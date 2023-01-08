cffd8163420951bbc37e9235e20bf5fd92cd9ca3
jaithecelt <111529559+jaithecelt@users.noreply.github.com>
2023-01-07 18:11:33 -0800

    Update FixDebugOne1.java

--- FixDebugOne1.java
+++ FixDebugOne1.java
@@ -3,5 +3,5 @@ public class FixDebugOne1
    /* This program displays a greeting */
    public static void main(String[] args)
    {
-      Systemoutprintln("Hello).
+      System.out.println("Hello).
    }
